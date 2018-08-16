acac848adc67f72c56fe554d6c9622a66c0bf27f
Nicolas Rollier <admin@iMac.local>
2018-08-17 00:19:40 +0200

    Switched to Semantic-UI & CDN

--- index.php
+++ index.php
@@ -5,24 +5,27 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="assets/css/bootstrap.css" />
-	<link rel="stylesheet" href="assets/css/font-awesome.css" />    
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.3/dist/semantic.min.css" />
 	<link rel="stylesheet" href="assets/css/style.css" />
 </head>
 
 <body>
-	<div class="alert alert-info" role="alert">
-		<button type="button" class="close" data-dismiss="alert">×</button>
-		<!-- If not running on localhost, define the correct URL ! --> 
-		<a href="javascript:window.location=%22http://localhost/bookmarklet/load.php?url=%22+encodeURIComponent(document.location)+%22&title=%22+encodeURIComponent(document.title)">
-			<i class="fa fa-bookmark"></i><span>Bookmark!</span></a>&nbsp;
-			Drag and drop the icon in your bookmarks bar + click the icon to bookmark a webpage in your local bookmark repository.
+	<div class="ui message">
+		<i class="close icon"></i>
+		<div class="ui breadcrumb">
+			<!-- If not running on localhost, define the correct URL ! -->
+			<a class="section" href="javascript:window.location=%22http://localhost/bookmarklet/load.php?url=%22+encodeURIComponent(document.location)+%22&title=%22+encodeURIComponent(document.title)">
+			<i class="bookmark icon"></i>Bookmark!</a><i class="right angle icon divider"></i>
+			<div class="section">Drag and drop the icon in your bookmarks bar + click the icon to bookmark a webpage in your local bookmark repository.</div>
 		</div>
-		<div class="container">
-		</br>
 	</div>
-	<script src="assets/js/jquery.js"></script>
-	<script src="assets/js/bootstrap.js"></script>
 
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+	<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.3/dist/semantic.min.js"></script>
+	<script type="text/javascript">
+	$('.message .close')
+		.on('click', function() {
+			$(this).closest('.message').transition('fade');});
+	</script>
 </body>
 </html>
